0da79f2e471044e65b3c6efe17eb6ad91a87148d
Thet Hmuu <thethmuueinsoe@gmail.com>
2023-04-12 17:01:56 +0630

    little style update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
+    <main className='text-center'>
       <h1>Home page</h1>
-      <Link href='/api/auth/signin'>login</Link>
+      <Link
+        className='text-blue-600 hover:underline hover:text-blue-700'
+        href='/api/auth/signin'
+      >
+        login
+      </Link>
     </main>
   );
 }
